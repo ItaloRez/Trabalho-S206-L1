fc7f6d8b21df8b6558721432ce9be29fd74d2f91
ItaloRez <italo.rezende@sharkdev.com.br>
2023-05-10 20:56:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Teste de Automação com Cypress no Site https://www.saucedemo.com
+
+Este é um projeto de teste de automação com o framework Cypress para o site https://www.saucedemo.com. Este projeto contém testes automatizados que verificam o comportamento da aplicação web em diferentes cenários de uso.
+
+## Instalação
+
+Para executar os testes, você precisa ter o Node.js e o Cypress instalados em sua máquina.
+
+Faça o clone deste repositório: git clone https://github.com/seu-usuario/nome-do-repositorio.git
+Instale as dependências: npm install
+Execute os testes: npm run cypress:open ou npm run cypress:spec
+Scripts de Execução
+Os seguintes scripts são fornecidos para facilitar a execução dos testes:
+
+npm run cypress:open: Abre a interface gráfica do Cypress para selecionar os testes que deseja executar.
+npm run cypress:spec: Executa todos os testes especificados no diretório cypress/e2e.
+
+## Estrutura do Projeto
+
+A estrutura do projeto é organizada da seguinte maneira:
+
+├── cypress
+│ ├── e2e
+│ │ ├── spec.cy.js
+├── .gitignore
+├── cypress.json
+├── package-lock.json
+├── package.json
+
+O projeto contém os seguintes testes automatizados:
+
+spec.cy.js: Testa o processo de login e compra na aplicação.
+
+## Conclusão
+
+Este projeto de teste de automação com Cypress para o site https://www.saucedemo.com é uma demonstração de como é possível criar testes automatizados para verificar o comportamento de uma aplicação web. O Cypress é uma ferramenta poderosa que permite escrever testes de forma clara e fácil de entender, e pode ser facilmente integrada a projetos existentes. Sinta-se à vontade para utilizar este projeto como ponto de partida para seus próprios testes de automação com Cypress!
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,78 @@
+describe("Criando cenários de teste para o site saucedemo", () => {
+  it("Caso de teste: Tentativa de login", () => {
+    cy.login("standard_user", "secret_sauce");
+
+    cy.get(".title").should("contain", "Products");
+  });
+
+  it("Caso de teste: Tentativa de login com usuário bloqueado", () => {
+    cy.login("locked_out_user", "secret_sauce");
+
+    cy.get('[data-test="error"]').should(
+      "contain",
+      "Sorry, this user has been locked out."
+    );
+  });
+
+  it("Caso de teste: Tentativa de login com usuário não cadastrado", () => {
+    cy.login("usuario_nao_cadastrado", "senha_nao_cadastrada");
+
+    cy.get('[data-test="error"]').should(
+      "contain",
+      "Username and password do not match any user in this service"
+    );
+  });
+
+  it("Caso de teste: Adicionar produto ao carrinho", () => {
+    cy.login("standard_user", "secret_sauce");
+
+    adicionarProdutoAoCarrinho("sauce-labs-backpack");
+
+    cy.get(".shopping_cart_link").click();
+
+    cy.get(".cart_item").should("contain", "Sauce Labs Backpack");
+  });
+
+  it("Caso de teste: Remover produto do carrinho", () => {
+    cy.login("standard_user", "secret_sauce");
+
+    adicionarProdutoAoCarrinho("sauce-labs-backpack");
+    adicionarProdutoAoCarrinho("sauce-labs-bike-light");
+
+    cy.get(".shopping_cart_link").click();
+
+    cy.get(".cart_item").should("contain", "Sauce Labs Backpack");
+
+    cy.get('[data-test="remove-sauce-labs-backpack"]').click();
+
+    cy.get(".cart_item").should("not.contain", "Sauce Labs Backpack");
+  });
+
+  it("Caso de teste: Finalizar compra", () => {
+    cy.login("standard_user", "secret_sauce");
+
+    adicionarProdutoAoCarrinho("sauce-labs-backpack");
+
+    cy.get(".shopping_cart_link").click();
+
+    cy.get(".cart_item").should("contain", "Sauce Labs Backpack");
+
+    cy.get('[data-test="checkout"]').click();
+
+    cy.get('[data-test="firstName"]').type("Teste");
+
+    cy.get('[data-test="lastName"]').type("Teste");
+
+    cy.get('[data-test="postalCode"]').type("00000000");
+
+    cy.get('[data-test="continue"]').click();
+
+    cy.get('[data-test="finish"]').click();
+
+    cy.get(".complete-header").should("contain", "Thank you for your order!");
+  });
+});
+
+function adicionarProdutoAoCarrinho(produto) {
+  cy.get(`[data-test="add-to-cart-${produto}"]`).click();
+}
